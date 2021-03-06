3588c128f9544a819366b422740431783ba7e249
Charles Oliver Nutter <headius@headius.com>
2021-03-05 19:28:26 -0600

    Modularize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.headius.backport9 {
+    requires java.base;
+
+    exports com.headius.backport9.buffer;
+    exports com.headius.backport9.modules;
+    exports com.headius.backport9.stack;
+}
